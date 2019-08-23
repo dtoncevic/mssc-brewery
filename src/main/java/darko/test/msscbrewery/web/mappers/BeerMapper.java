e4e558f59b0b350aafe97c622d16ede9c247c35a
Darko Toncevic <dtoncevic@gmail.com>
2019-08-23 15:08:48 +0200

    Added custom mappers.

--- src/main/java/darko/test/msscbrewery/web/mappers/BeerMapper.java
+++ src/main/java/darko/test/msscbrewery/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import darko.test.msscbrewery.domain.Beer;
 import darko.test.msscbrewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = DateMapper.class)
 public interface BeerMapper {
 
     BeerDto beerToBeerDto(Beer beer);
